1ab6e2983290f61a88c6d78a2a49de56dad97b17
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-08-22 18:47:20 +0530

    form

--- form/form.py
+++ form/form.py
@@ -1,7 +1,13 @@
 from django import forms
+from .models import *
 
-class Registration(forms.Form):
-    fname=forms.CharField(max_length=50,label="First name")
-    lname=forms.CharField(max_length=20,label="Last name")
-    email=forms.EmailField(label="Email")
-    contact=forms.IntegerField(label="Contact")
+# class Registration(forms.Form):
+#     fname=forms.CharField(max_length=50,label="First name")
+#     lname=forms.CharField(max_length=20,label="Last name")
+#     email=forms.EmailField(label="Email")
+#     contact=forms.IntegerField(label="Contact")
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
--- form/views.py
+++ form/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import render
 from .form import *
+from .models import *
 
 # Create your views here.
 
+# def home(request):
+#     form=Registration()
+#     # return render(request,"home.html",{"form":form})
+#     if request.method=="POST":
+#         form=Registration(request.POST)
+        
+#         if form.is_valid():
+#             fname=form.cleaned_data["fname"]
+#             lname=form.cleaned_data["lname"]
+#             email=form.cleaned_data["email"]
+#             contact=form.cleaned_data["contact"]
+#             print(fname,lname,email,contact)
+#             # data={"fname":fname,"lname":lname,"email":email,"contact":contact}
+#     return render(request,"home.html",{"form":form})
+
+
 def home(request):
-    form=Registration()
+    form=StudentForm()
     # return render(request,"home.html",{"form":form})
     if request.method=="POST":
-        form=Registration(request.POST)
-        data=None
+        form=StudentForm(request.POST)
         
         if form.is_valid():
-            fname=form.cleaned_data["fname"]
-            lname=form.cleaned_data["lname"]
+            name=form.cleaned_data["name"]
+            city=form.cleaned_data["city"]
             email=form.cleaned_data["email"]
             contact=form.cleaned_data["contact"]
-            print(fname,lname,email,contact)
-            data={"fname":fname,"lname":lname,"email":email,"contact":contact}
+            print(name,city,email,contact)
+            # data={"fname":fname,"lname":lname,"email":email,"contact":contact}
+            # Student.objects.create(Stu_name=name,Stu_city=city,Stu_email=email,Stu_contact=contact)
+            form.save()
     return render(request,"home.html",{"form":form})
